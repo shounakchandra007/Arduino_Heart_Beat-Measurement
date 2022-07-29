e8b68bb3c08c351772377bf062c383758ed68924
Shounak Chandra <56895242+shounakchandra007@users.noreply.github.com>
2022-07-29 21:41:31 +0530

    Change_Code
    Change_Code

--- Heart_Beat_Measurement.ino
+++ Heart_Beat_Measurement.ino
@@ -1,5 +1,5 @@
 //Heart Pulse Sensor
-//Programmed By @Shounak Chandra
+//Programmed By @Shounak Chandra_DEvelopment
 #include <LiquidCrystal.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
